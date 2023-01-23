306ba04b741596c85c16ca6edba89e2c80550eb7
Wei Xin Yuan <weixinyuan6@gmail.com>
2023-01-23 20:51:22 +0000

    add dev_nimble_install.sh

---
+++ dev_nimble_install.sh
@@ -0,0 +1,2 @@
+rm -rf ~/.nimble/bin/yexe && rm -rf ~/.nimble/bin/lexim && rm -rf  ~/.nimble/pkgs2/nimyacc* && rm -rf ~/.nimble/pkgs2/lexim*  && rm -rf nimcache && rm -rf ~/.cache/nim && nimble install
+
